1f1760b7ab829f183eada5f15ab826a0eb980e6b
RokasV <vezikauskas.r@gmail.com>
2024-02-28 09:03:46 +0200

    button props

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,11 +1,24 @@
-export default function Button({ onClick = () => {}, children, outline, type = 'button' }) {
+type ButtonProps = {
+  onClick?: () => void;
+  children: string;
+  outline?: boolean;
+  type?: 'button' | 'submit' | 'reset';
+};
+
+export default function Button({
+  onClick = () => {},
+  children,
+  outline,
+  type = 'button',
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
       className={`${
         outline ? 'bg-white text-indigo-500' : 'bg-indigo-500 text-white'
       }  px-6 py-2 text-sm uppercase border font-semibold border-indigo-500 rounded-md`}
-      type={type}>
+      type={type}
+    >
       {children}
     </button>
   );
