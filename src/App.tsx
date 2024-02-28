870bd459187cf6082a7c579db6c84435e65df348
RokasV <vezikauskas.r@gmail.com>
2024-02-28 08:54:32 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='container border-2'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
